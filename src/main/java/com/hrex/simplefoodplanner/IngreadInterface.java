1e8d162725886e01c78f0a188dc15ac8af5141dc
Harrison <harrisonmacdonald79@googlemail.com>
2025-02-02 23:27:19 +0000

    moving ingredients loading to a new file

---
+++ src/main/java/com/hrex/simplefoodplanner/IngreadInterface.java
@@ -0,0 +1,59 @@
+package com.hrex.simplefoodplanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngreadInterface {
+
+	private static ArrayList<Ingreadient> ingsIn = new ArrayList<Ingreadient>();
+
+
+//Loads the ingreadients from file
+  public static void loadIngreads() {
+    try {
+      File myObj = new File("src\\main\\resources\\ingredients.txt");
+      Scanner sc = new Scanner(myObj);
+      while (sc.hasNextLine()) {
+        String data = sc.nextLine();
+        addIng(data);
+      }
+      sc.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
+  }
+  
+  //Adds the lines to the arraylist
+  public static void addIng(String line) {
+    int[] comLocations = new int[3];
+    String temp = line;
+    int j = 0;
+    
+    for(int i = 0; i < 3 -1; i++) {
+      while(temp.charAt(0)!=',') {
+        temp = temp.substring(1);
+        j++;
+      }
+      j++;
+      temp = temp.substring(1);
+      comLocations[i] = j;
+    }
+    
+    boolean inStock = Boolean.parseBoolean(line.substring(0,comLocations[0]));
+    String name = line.substring(comLocations[0],comLocations[1]-1);
+    double amountStock = Double.parseDouble(line.substring(comLocations[1])); 
+    
+    ingsIn.add(new Ingreadient(inStock, name, amountStock));
+  }
+
+  //tsd
+  public static List<Ingreadient> getIngredients(){
+      loadIngreads();
+      return ingsIn;
+  }
+  
+}
--- src/main/java/com/hrex/simplefoodplanner/Start.java
+++ src/main/java/com/hrex/simplefoodplanner/Start.java
@@ -1,56 +1,12 @@
 package com.hrex.simplefoodplanner;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Start {
-	
-	private static ArrayList<Ingreadient> ingsIn = new ArrayList<Ingreadient>();
-	
 	public static void main(String[] args){
-		loadIngreads();
+	
+	System.out.println(IngreadInterface.getIngredients());
 		
 		
 		//makeNow = new ArrayList();
 		//shopList = new ArrayList();
-	}
-	
-	public static void loadIngreads() {
-		try {
-			File myObj = new File("ings.txt");
-			Scanner sc = new Scanner(myObj);
-			while (sc.hasNextLine()) {
-				String data = sc.nextLine();
-				addIng(data);
-			}
-			sc.close();
-		} catch (FileNotFoundException e) {
-				System.out.println("An error occurred.");
-				e.printStackTrace();
-		}
-	}
-	
-	public static void addIng(String line) {
-		int[] comLocations = new int[3];
-		String temp = line;
-		int j = 0;
-		
-		for(int i = 0; i < 3 -1; i++) {
-			while(temp.charAt(0)!=',') {
-				temp = temp.substring(1);
-				j++;
-			}
-			j++;
-			temp = temp.substring(1);
-			comLocations[i] = j;
-		}
-		
-		boolean inStock = Boolean.parseBoolean(line.substring(0,comLocations[0]));
-		String name = line.substring(comLocations[0],comLocations[1]-1);
-		double amountStock = Double.parseDouble(line.substring(comLocations[1])); 
-		
-		ingsIn.add(new Ingreadient(inStock, name, amountStock));
 	}
 }
