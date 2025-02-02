ef82d815275d01751980c8656e43328809581bc1
Harrison <harrisonmacdonald79@googlemail.com>
2025-02-02 23:04:49 +0000

    begining of making dish

--- src/main/java/com/hrex/simplefoodplanner/Dish.java
+++ src/main/java/com/hrex/simplefoodplanner/Dish.java
@@ -3,11 +3,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dish {
-	private List<Ingreadient> dishes;
+	private List<Ingreadient> ingreeds;
+	private String name;
 		
 		
-		public Dish(){
-			this.dishes = new ArrayList<Ingreadient>();
+		public Dish(String name){
+			this.name = name;
+			this.ingreeds = new ArrayList<Ingreadient>();
 		}
+
+		public void addIng(Ingreadient ingToAdd){
+			//TODO: This requires checks
+			this.ingreeds.add(ingToAdd);
+		}
+
+		public String getName(){
+			return this.name;
+		}
+
+
 
 	}
