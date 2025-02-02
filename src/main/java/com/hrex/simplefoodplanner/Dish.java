9ab1eafdb3ed962060816f42a33d4ad200cfc575
Harrison <harrisonmacdonald79@googlemail.com>
2025-02-02 22:30:02 +0000

    This is the the new branch with maven

---
+++ src/main/java/com/hrex/simplefoodplanner/Dish.java
@@ -0,0 +1,13 @@
+package com.hrex.simplefoodplanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dish {
+	private List<Ingreadient> dishes;
+		
+		
+		public Dish(){
+			this.dishes = new ArrayList<Ingreadient>();
+		}
+
+	}
---
+++ src/test/java/mainTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.hrex.simplefoodplanner.Dish;
+
+
+public class mainTest{
+
+  @BeforeEach
+  void setUpTests() {
+    //before each test
+  }
+
+  @Test
+  void testDish(){  // I am testing that i can make a dish
+    Dish mumSauce = new Dish();
+  }
+
+}
