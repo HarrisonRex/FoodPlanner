6d249486366d971402881ec95749de0a20ab59fe
Harrison <harrisonmacdonald79@googlemail.com>
2025-02-03 00:15:15 +0000

    made base of recipe

--- src/main/java/com/hrex/simplefoodplanner/recipe.java
+++ src/main/java/com/hrex/simplefoodplanner/recipe.java
@@ -1,5 +1,27 @@
 package com.hrex.simplefoodplanner;
 
-public class recipe {
+import java.util.*;
+public class Recipe {
+  private String name;
+  public String getName() {
+    return name;
+  }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Ingreadient> getIngreadients() {
+    return ingreadients;
+  }
+
+  public void setIngreadients(List<Ingreadient> ingreadients) {
+    this.ingreadients = ingreadients;
+  }
+
+  private List<Ingreadient> ingreadients;
+
+  public Recipe(String name){
+    this.name = name;
+  }
 }
