2650113d96e947d2307cdc1ee630ee54ad4dc471
Harrison <harrisonmacdonald79@googlemail.com>
2025-02-03 00:08:33 +0000

    making of recipe and test class

---
+++ src/main/java/com/hrex/simplefoodplanner/recipe.java
@@ -0,0 +1,5 @@
+package com.hrex.simplefoodplanner;
+
+public class recipe {
+
+}
---
+++ src/test/java/com/hrex/simplefoodplanner/recipeTest.java
@@ -0,0 +1,18 @@
+package com.hrex.simplefoodplanner;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class recipeTest {
+  
+  @BeforeEach
+  void setUpTests() {
+    //before each test
+  }
+
+
+  @Test
+  void testMakeRecipe(){ // making a recipe object
+    
+  }
+}
