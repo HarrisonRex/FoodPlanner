10ee9f68db2ad3efbfde34e333060daa5ec2f434
Harrison <harrisonmacdonald79@googlemail.com>
2025-02-06 01:51:26 +0000

    making interface test class

--- src/main/java/com/hrex/simplefoodplanner/Start.java
+++ src/main/java/com/hrex/simplefoodplanner/Start.java
@@ -1,11 +1,13 @@
 package com.hrex.simplefoodplanner;
 
+import java.util.ArrayList;
+
 import com.hrex.simplefoodplanner.interfaces.IngreadientInterface;
 import com.hrex.simplefoodplanner.interfaces.RecipeInterface;
 
 public class Start {
 	public static void main(String[] args){
-	
+
 		//System.out.println(IngreadientInterface.getIngredients());
 		System.out.println(RecipeInterface.getRecipes());
 		
---
+++ src/test/java/com/hrex/simplefoodplanner/InterfaceTests.java
@@ -0,0 +1,18 @@
+package com.hrex.simplefoodplanner;
+
+import org.junit.jupiter.api.Test;
+
+public class InterfaceTests {
+
+  //both need tests for loading too 
+
+  @Test
+  void testSaveRecipes() {
+    
+  }
+
+  @Test
+  void testSaveIngreadiants() {
+
+  }
+}
