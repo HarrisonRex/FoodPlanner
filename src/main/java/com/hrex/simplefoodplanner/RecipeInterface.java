5e6703b00d7affa80a94f71af9567dfa0ced3a1c
Harrison <harrisonmacdonald79@googlemail.com>
2025-02-06 01:05:47 +0000

    creating of the class and branch

---
+++ src/main/java/com/hrex/simplefoodplanner/RecipeInterface.java
@@ -0,0 +1,17 @@
+package com.hrex.simplefoodplanner;
+
+package com.hrex.simplefoodplanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import com.hrex.simplefoodplanner.etc.units;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeInterface {
+
+}
